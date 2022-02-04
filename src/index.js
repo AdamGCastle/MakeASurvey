77b916353d3ac5cdadd4825f4021c980218fe731
AdamGCastle <88667052+AdamGCastle@users.noreply.github.com>
2022-02-05 01:08:37 +0200

    tryAgain

--- src/index.js
+++ src/index.js
@@ -6,10 +6,8 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <BrowserRouter>
-  <div className='main'>
-    <App />
-  </div>    
+  <BrowserRouter>  
+    <App />     
   </BrowserRouter>,
   document.getElementById('root')
 );
